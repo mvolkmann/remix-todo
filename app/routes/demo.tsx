a410da01abb5fe52cc5a178ab23dca954568e579
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-10-14 15:29:47 -0500

    progress

--- app/routes/demo.tsx
+++ app/routes/demo.tsx
@@ -3,13 +3,7 @@ import type {LinksFunction, MetaFunction} from '@remix-run/node';
 // Don't need file extension when importing JS/TS files.
 import Heading, {links as headingLinks} from '~/components/Heading';
 
-// If you placed demo.css in the "routes" directory, you will get the error
-// "Cannot read properties of undefined (reading 'filter')".
-// Do need file extension when importing CSS files.
-import styles from '~/styles/demo.css?url';
-
 export const links: LinksFunction = () => [
-  {rel: 'stylesheet', href: styles},
   // This pattern is called "surfacing links" in the Remix docs.
   ...headingLinks()
 ];
